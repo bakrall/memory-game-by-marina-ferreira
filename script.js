4d7f54deb0e70d3d6b31aad0833a3a7a040e1865
Barbara Stadnik <barbara1stadnik@gmail.com>
2019-01-06 14:40:40 +0100

    store which card was clicked (in pairs)

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 const cards = document.querySelectorAll('.memory-card');
 
+let hasFlippedCard = false;
+let firsCard, secondCard;
+
 cards.forEach(card => card.addEventListener('click', flipCard));
 
 function flipCard() {
 	this.classList.toggle('flip');
+
+	if (!hasFlippedCard) {
+		//first click
+		hasFlippedCard = true;
+		firstCard = this;
+	} else {
+		//second click
+		hasFlippedCard = false;
+		secondCard = this;
+
+		//do cards match?
+		console.log(firstCard.dataset.framework);
+		console.log(secondCard.dataset.framework);
+	}
 }
